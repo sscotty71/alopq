939d4f28aa4c3d7fc69d2d6f22ec78e195d0e332
Enrico Pirozzi <sscotty71@gmail.com>
2019-07-25 10:35:10 +0200

    Update vacuum.sql

--- admin_queries/vacuum.sql
+++ admin_queries/vacuum.sql
@@ -2,3 +2,6 @@
 -- check for dead rows / vacuum / autovacuum
 select schemaname,relname,n_dead_tup,n_live_tup, n_mod_since_analyze, n_tup_hot_upd,last_autovacuum,last_autoanalyze,autovacuum_count, autoanalyze_count  
 from pg_stat_all_tables order by n_dead_tup desc;
+
+--Last autovacuums
+SELECT * FROM pg_stat_user_tables where last_autovacuum is not null order by last_autovacuum desc;
